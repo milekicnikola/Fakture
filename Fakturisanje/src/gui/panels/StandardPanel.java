c0c6f3c2390c7b67423ab08ee39ba9886efbbf21
Nikola Milekic <milekic.nikola@gmail.com>
2018-06-14 21:08:14 +0200

    Implementirano povezivanje na server i omoguceno pravljenje .jar i .exe fajlova.

--- Fakturisanje/src/gui/panels/StandardPanel.java
+++ Fakturisanje/src/gui/panels/StandardPanel.java
@@ -1,10 +1,14 @@
 package gui.panels;
 
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
+import util.ResourceLoader;
 
 public abstract class StandardPanel extends JPanel {
 
@@ -24,10 +28,24 @@ public abstract class StandardPanel extends JPanel {
 		btnConfirm = new JButton();
 		btnCancel = new JButton();
 
-		btnConfirm.setIcon(new ImageIcon("Images/commit.png"));
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(ResourceLoader.load("Images/commit.png"));			
+		} catch (Exception e) {			
+
+		}		
+		
+		BufferedImage image1 = null;
+		try {
+			image1 = ImageIO.read(ResourceLoader.load("Images/cancel.png"));			
+		} catch (Exception e) {			
+
+		}
+		
+		btnConfirm.setIcon(new ImageIcon(image));
 		btnConfirm.setToolTipText("Potvrdi");
 
-		btnCancel.setIcon(new ImageIcon("Images/cancel.png"));
+		btnCancel.setIcon(new ImageIcon(image1));
 		btnCancel.setToolTipText("Odustani");
 
 		btnConfirm.setEnabled(false);
